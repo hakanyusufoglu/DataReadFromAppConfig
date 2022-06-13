a3694bf9fe6438e8f951d8356da49f86484c868d
hyusufoglu <hyusufoglu@isbak.istanbul>
2022-06-13 10:18:23 +0300

    Add project files.

---
+++ DataReadFromAppConfig.ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Configuration;
+
+namespace DataReadFromAppConfig.ConsoleApp
+{
+    class Program
+    {
+        private static string uniqueKey = ConfigurationManager.AppSettings["UniqueKey"];
+        private static string webSite = ConfigurationManager.AppSettings["googleWebSiteKey"];
+        private static int number = Convert.ToInt32(ConfigurationManager.AppSettings["numberKey"]);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Appconfig first key: "+uniqueKey+"\n"+"Appconfig second key: "+webSite + "\n" + "Appconfig fourth key: " + number);
+            Console.ReadLine();
+        }
+    }
+}
